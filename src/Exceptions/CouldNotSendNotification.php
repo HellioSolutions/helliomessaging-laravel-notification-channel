7efb8a789253707a5c4deccd5b4315794eee6902
HellioMessaging <74058389+HellioMessaging@users.noreply.github.com>
2020-11-09 15:04:27 +0100

    Update CouldNotSendNotification.php
    added exception message for message type

--- src/Exceptions/CouldNotSendNotification.php
+++ src/Exceptions/CouldNotSendNotification.php
@@ -11,11 +11,16 @@ class CouldNotSendNotification extends \Exception
 
     public static function msisdnNotSetError()
     {
-        return new static('Recipient phone number not set');
+        return new static('Recipient mobile number not set');
     }
 
     public static function messageNotSetError()
     {
         return new static('Message content empty');
+    }
+    
+    public static function messageTypeNotSetError()
+    {
+        return new static('You must set message type to either 0 or 1');
     }
 }
